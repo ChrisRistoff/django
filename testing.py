273a0dfa17a914c86a25289fa8d96faa91c0d700
krasenHristov <doanusho@gmail.com>
2023-04-08 20:42:06 +0100

    actions test

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-
+# adding a comment to test github actions
 class LinkedList:
     def __init__(self):
         self.head = None
